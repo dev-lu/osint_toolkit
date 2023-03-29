8863d785efd387f47ed5deb27482f30c4facb414
dev-lu <44299200+dev-lu@users.noreply.github.com>
2023-03-29 18:45:06 +0200

    Redesigned component

--- frontend/src/components/ioc-analyzer/services/Reddit.jsx
+++ frontend/src/components/ioc-analyzer/services/Reddit.jsx
@@ -1,62 +1,148 @@
-import React from 'react'
+import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+import { AccessTime, Link, Person, Score } from "@mui/icons-material";
+
+import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
+import Divider from "@mui/material/Divider";
 import Box from "@mui/material/Box";
-import Stack from '@mui/material/Stack';
+import Stack from "@mui/material/Stack";
+import { Typography } from "@mui/material";
 
 import ResultRow from "../ResultRow";
-
 
 export default function Reddit(props) {
   const [result, setResult] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [expandedStates, setExpandedStates] = useState([]);
+
+  const toggleExpanded = (index) => {
+    const newExpandedStates = [...expandedStates];
+    newExpandedStates[index] = !newExpandedStates[index];
+    setExpandedStates(newExpandedStates);
+  };
+
+  useEffect(() => {
+    if (result) {
+      setExpandedStates(new Array(result.length).fill(false));
+    }
+  }, [result]);
+
+  const getTruncatedMessage = (message, maxLength) => {
+    if (message.length > maxLength) {
+      return message.slice(0, maxLength) + "...";
+    }
+    return message;
+  };
 
   useEffect(() => {
     const fetchData = async () => {
-        setLoading(true);
-        try {
-            const url = 'http://localhost:8000/api/socialmedia/reddit/' + props.ioc;
-            const response = await axios.get(url);
-            setResult(response.data);
-        } catch (e) {
-            setError(e);
-        }
-        setLoading(false);
-    }
+      setLoading(true);
+      try {
+        const url = "http://localhost:8000/api/socialmedia/reddit/" + props.ioc;
+        const response = await axios.get(url);
+        setResult(response.data);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
     fetchData();
-}, []);
+  }, []);
 
-const details = (
-  <>
-    {result && result.length > 0 ? (
-      <Box sx={{ margin: 1 }}>
-        {result.map((post) => (
-          <Card elevation={0} variant="outlined" sx={{ m: 1.5, p: 2, borderRadius: 5 }} key={post.id}>
-            <Stack direction="row" spacing={2} width={'100%'} alignItems="flex-start">
-              <Stack direction="column" width="100%">
-                <p><b>Author: </b>{post.author}</p>
-                <p><b>Score: </b>{post.score}</p>
-                <p><b>Title: </b>{post.title}</p>
+  const details = (
+    <>
+      {result && result.length > 0 ? (
+        <Box sx={{ margin: 1 }}>
+          {result.map((post, index) => (
+            <Card
+              key={post.id}
+              variant="outlined"
+              sx={{ m: 1.5, p: 2, borderRadius: 5 }}
+            >
+              <Stack direction="row" alignItems="flex-start">
+                <Box sx={{ flexGrow: 1 }}>
+                  <Typography variant="h6" component="h2">
+                    {post.title}
+                  </Typography>
+                  <Stack direction="row">
+                    <Typography
+                      variant="subtitle1"
+                      component="div"
+                      color="textSecondary"
+                      style={{ display: "flex", alignItems: "center" }}
+                    >
+                      <Person fontSize="small" sx={{ mr: 1 }} /> {post.author}
+                      <Divider
+                        orientation="vertical"
+                        flexItem
+                        variant="middle"
+                        style={{ marginLeft: 8, marginRight: 8 }}
+                      />{" "}
+                      <Score fontSize="small" sx={{ mr: 1 }} /> {post.score}{" "}
+                      points
+                      <Divider
+                        orientation="vertical"
+                        flexItem
+                        variant="middle"
+                        style={{ marginLeft: 8, marginRight: 8 }}
+                      />{" "}
+                      <AccessTime fontSize="small" sx={{ mr: 1 }} />{" "}
+                      {post.created_utc}
+                    </Typography>
+                  </Stack>
+                  <Typography variant="body1" component="p" sx={{ my: 2 }}>
+                    {expandedStates[index]
+                      ? post.message
+                      : getTruncatedMessage(post.message, 200)}
+                    {post.message && post.message.length > 200 && (
+                      <Button
+                        onClick={() => toggleExpanded(index)}
+                        sx={{ mt: 1 }}
+                      >
+                        {expandedStates[index] ? "Read less" : "Read more"}
+                      </Button>
+                    )}
+                  </Typography>
+                  <Button
+                    variant="outlined"
+                    href={post.url}
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    Go to post
+                  </Button>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    ml: 2,
+                  }}
+                >
+                  <Typography
+                    variant="caption"
+                    component="p"
+                    color="textSecondary"
+                  >
+                    ID: {post.id}
+                  </Typography>
+                </Box>
               </Stack>
-              <Stack direction="column" sx={{ justifyContent: 'flex-end' }}>
-                <p><b>Date: </b>{post.created_utc}</p>
-                <p><b>URL: </b>{post.url}</p>
-              </Stack>
-            </Stack>
-            <br />
-            {post.message !== "" ? <p>{post.message}</p> : <p>No message text...</p>}
-          </Card>
-        ))}
-      </Box>
-    ) : (
-      "No results"
-    )}
-  </>
-);
-
+            </Card>
+          ))}
+        </Box>
+      ) : (
+        <Typography variant="body1" component="p">
+          No results
+        </Typography>
+      )}
+    </>
+  );
 
   return (
     <>
@@ -67,15 +153,15 @@ const details = (
         loading={loading}
         result={result}
         summary={
-          result && result.length > 0 ? "Show latest " + result.length + " posts" : "No posts found"
+          result && result.length > 0
+            ? "Show latest " + result.length + " posts"
+            : "No posts found"
         }
         summary_color={{ color: null }}
-        color={
-          result && result.length > 0 ? "orange" : "green"
-        }
+        color={result && result.length > 0 ? "orange" : "green"}
         error={error}
         details={details}
       />
     </>
-  )
+  );
 }
