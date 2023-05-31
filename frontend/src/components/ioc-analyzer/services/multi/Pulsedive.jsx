8d56e0f41a7df1df5d1d562024f1855df449c625
dev-lu <44299200+dev-lu@users.noreply.github.com>
2023-05-31 16:16:16 +0200

    Improved TLP logic

--- frontend/src/components/ioc-analyzer/services/multi/Pulsedive.jsx
+++ frontend/src/components/ioc-analyzer/services/multi/Pulsedive.jsx
@@ -1,64 +1,78 @@
-import React from 'react';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
-import NoDetails from '../NoDetails';
+import NoDetails from "../NoDetails";
 import ResultRow from "../../ResultRow";
 
 export default function Pulsedive(props) {
-    const [result, setResult] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const url = 'http://localhost:8000/api/' + props.type + '/pulsedive?ioc=' + encodeURIComponent(props.ioc);
-                const response = await axios.get(url);
-                setResult(response.data);
-            } catch (e) {
-                setError(e);
-            }
-            setLoading(false);
-        }
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const url =
+          "http://localhost:8000/api/" +
+          props.type +
+          "/pulsedive?ioc=" +
+          encodeURIComponent(props.ioc);
+        const response = await axios.get(url);
+        setResult(response.data);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
 
-    const details = (
-        <>
-            {result ? (<Box sx={{ margin: 1 }}>
-                <Grid display="flex" justifyContent="center" alignItems="center">
-                    <NoDetails />
-                </Grid>
-            </Box>) : null}
-        </>
-    )
+  const details = (
+    <>
+      {result ? (
+        <Box sx={{ margin: 1 }}>
+          <Grid display="flex" justifyContent="center" alignItems="center">
+            <NoDetails />
+          </Grid>
+        </Box>
+      ) : null}
+    </>
+  );
 
   return (
     <>
-        <ResultRow
+      <ResultRow
         name="Pulsedive"
         id="pulsedive"
         icon="pulsedive_logo_small"
         loading={loading}
         result={result}
         summary={
-            result != null && result['results'][0] ? 
-            "Risk: " + result['results'][0]['risk'].charAt(0).toUpperCase() +
-            result['results'][0]['risk'].slice(1) : 'Not found'
-            }
+          result != null && result["results"][0]
+            ? "Risk: " +
+              result["results"][0]["risk"].charAt(0).toUpperCase() +
+              result["results"][0]["risk"].slice(1)
+            : "Not found"
+        }
         summary_color={{ color: null }}
         color={
-            result != null && result['results'][0] ?
-            (result['results'][0]['risk'] === 'none' ? 'green' : result['results'][0]['risk'] === 'low' ? 'orange' : 'red') : 'lightgrey'
+          result != null && result["results"][0]
+            ? result["results"][0]["risk"] === "none"
+              ? "green"
+              : result["results"][0]["risk"] === "low" ||
+                result["results"][0]["risk"] === "medium"
+              ? "orange"
+              : "red"
+            : "lightgrey"
         }
         error={error}
         details={details}
       />
     </>
-  )
+  );
 }
