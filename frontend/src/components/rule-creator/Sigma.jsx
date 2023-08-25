1ffa42f4205f7455a469653044a8b3972490bc5a
dev-lu <44299200+dev-lu@users.noreply.github.com>
2023-08-25 22:52:06 +0200

    Add reset button

--- frontend/src/components/rule-creator/Sigma.jsx
+++ frontend/src/components/rule-creator/Sigma.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useSetRecoilState, useRecoilValue } from "recoil";
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { v4 as uuidv4 } from "uuid";
 import GeneralInformation from "./GeneralInformation";
 import References from "./References";
@@ -11,6 +11,7 @@ import Fields from "./Fields";
 import Falsepositives from "./Falsepositives";
 import Export from "./Export";
 import { RuleIdAtom } from "./SigmaAtom";
+import Reset from "./Reset";
 
 export default function Sigma() {
   const setRuleId = useSetRecoilState(RuleIdAtom);
@@ -22,12 +23,20 @@ export default function Sigma() {
 
   return (
     <>
-      <Typography variant="h5" gutterBottom mt={2}>
-        Create new Sigma Rule
-      </Typography>
-      <Typography variant="h7" gutterBottom mb={2}>
-        ID: {ruleId}
-      </Typography>
+      <Grid container justifyContent="space-between" alignItems="center">
+        <Grid item xs={11}>
+          <Typography variant="h5" gutterBottom mt={2}>
+            Create new Sigma Rule
+          </Typography>
+          <Typography variant="h7" gutterBottom mb={2}>
+            ID: {ruleId}
+          </Typography>
+        </Grid>
+
+        <Grid item xs={1} container justifyContent="flex-end">
+          <Reset />
+        </Grid>
+      </Grid>
 
       <GeneralInformation />
 
