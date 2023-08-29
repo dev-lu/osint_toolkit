b888aaa72615057de9cca5f59a5717d78c12cca6
dev-lu <44299200+dev-lu@users.noreply.github.com>
2023-08-29 12:37:22 +0200

    Change database location
    Change database location and map database to local file system

--- backend/database/database.py
+++ backend/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database/sql_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database/data/otk_data.db"
 
 # Create the database engine
 engine = create_engine(
